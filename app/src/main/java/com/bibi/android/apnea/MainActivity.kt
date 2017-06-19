90574a8a771832226c76e3435f4473913ac1b9c0
Bianca Del Carretto <delca85@gmail.com>
2017-06-19 10:16:53 +0200

    aggiunta richiesta conferma reset record

--- app/src/main/java/com/bibi/android/apnea/MainActivity.kt
+++ app/src/main/java/com/bibi/android/apnea/MainActivity.kt
@@ -334,13 +334,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
         alertDialogBuilder.setPositiveButton("Reset", {
-            dialog, which ->  removeFileAndLine(tableLayout)
-            if (record != null)
-                Toast.makeText(applicationContext, "Record deleted!",
+            dialog, which -> confirmationDialog(tableLayout, record)
+            })
+        val alert = alertDialogBuilder.create()
+    }
+
+    private fun  confirmationDialog(tableLayout: TableLayout, record: String?) {
+        val layoutInflater = layoutInflater
+        var view = layoutInflater.inflate(R.layout.confirmation_reset_dialog, null)
+        var alertDialogBuilder = AlertDialog.Builder(this@MainActivity)
+        alertDialogBuilder.setView(view)
+
+        alertDialogBuilder.setPositiveButton("Yes", {
+            dialog, which ->
+            if (record != null) {
+                removeFileAndLine(tableLayout)
+            Toast.makeText(applicationContext, "Record deleted!",
                     Toast.LENGTH_LONG).show()
+            }
             else
                 Toast.makeText(applicationContext, "Nothing to be deleted!",
-                    Toast.LENGTH_LONG).show()})
+                        Toast.LENGTH_LONG).show()})
+        alertDialogBuilder.setNegativeButton("No", {
+            dialog, which ->
+                Toast.makeText(applicationContext, "Nothing deleted!",
+                    Toast.LENGTH_LONG).show()
+        })
         val alert = alertDialogBuilder.create()
         alert.show()
     }
